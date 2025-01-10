a015b9ed4b7aea750e51a74fd34ecd7228a08d5b
lazy-abhay <abhaykrn02@gmail.com>
2025-01-10 19:37:27 +0530

    10th_Jan Solved Problems

---
+++ CodeForces/January/10th_Jan/1674D.cpp
@@ -0,0 +1,42 @@
+/*     Coded by lazy_abhay     */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+void solve(int __test_case)
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (auto &it : arr)
+        cin >> it;
+    for (int i = (n & 1); i < n; i += 2)
+        if (arr[i] > arr[i + 1])
+            swap(arr[i], arr[i + 1]);
+    if (is_sorted(all(arr)))
+        cout << "YES";
+    else
+        cout << "NO";
+}
+
+int32_t main(void)
+{
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int __test_case = 1;
+    cin >> __test_case;
+
+    for (int __t = 1; __t <= __test_case; ++__t)
+    {
+        solve(__t);
+        cout << endl;
+    }
+
+    return 0;
+}
