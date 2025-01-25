9adaa3ac543c908afb294fb838f22ab890785f1a
lazy-abhay <abhaykrn02@gmail.com>
2025-01-25 22:02:01 +0530

    25th Jan Solved Problems

---
+++ CodeForces/January/25th_Jan/1926D.cpp
@@ -0,0 +1,45 @@
+/*     Coded by lazy_abhay     */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int32_t main(void)
+{
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    auto solve = [&](int __t) -> void
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (auto &it : arr)
+            cin >> it;
+        map<int, int> mp;
+        int ans = 0;
+        for (auto &it : arr)
+        {
+            if (mp[it ^ ((1LL << 31) - 1)])
+                mp[it ^ ((1LL << 31) - 1)]--;
+            else
+                mp[it]++, ans++;
+        }
+        cout << ans;
+    };
+
+    int __T = 1;
+    cin >> __T;
+
+    for (int __t = 1; __t <= __T; __t++)
+    {
+        solve(__t);
+        cout << endl;
+    }
+
+    return 0;
+}
