bfe2f557c8eed986556ca708e65d77455ee2cd32
Abhay <abhaykrn02@gmail.com>
2025-03-04 17:38:43 +0530

    4th March Solved Problemsj

---
+++ CodeForces/March/4th/2002B.cpp
@@ -0,0 +1,46 @@
+/*     Coded by lazy_abhay     */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int32_t main(void)
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    auto abhay = [&](int __t) -> void
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n), b(n);
+        for (auto &it : a)
+            cin >> it;
+        for (auto &it : b)
+            cin >> it;
+        if (a == b)
+            cout << "Bob";
+        else
+        {
+            reverse(all(a));
+            if (a == b)
+                cout << "Bob";
+            else
+                cout << "Alice";
+
+        }
+    };
+
+    int T = 1;
+    cin >> T;
+    for (int t = 1; t <= T; ++t)
+    {
+        abhay(t);
+        cout << endl;
+    }
+    return 0;
+}
