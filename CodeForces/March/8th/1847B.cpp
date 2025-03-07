31a2725af937a378fc04845177b24e8217236970
Abhay <abhaykrn02@gmail.com>
2025-03-08 03:54:28 +0530

    8th March Solved Problems

---
+++ CodeForces/March/8th/1847B.cpp
@@ -0,0 +1,42 @@
+/*     Coded by lazy_abhay     */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int32_t main(void)
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    auto abhay = [&](int __t) -> void
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (auto &it : arr)
+            cin >> it;
+        int ans = 0;
+        int And = -1;
+        for (auto &it : arr)
+        {
+            And &= it;
+            if (And == 0)
+                ans++, And = -1;
+        }
+        cout << max(1LL, ans); 
+    };
+
+    int T = 1;
+    cin >> T;
+    for (int t = 1; t <= T; ++t)
+    {
+        abhay(t);
+        cout << endl;
+    }
+    return 0;
+}
