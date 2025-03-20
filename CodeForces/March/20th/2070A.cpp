41aa5b3f23ebdf6233ff32ad695e2a816f2c2dad
Abhay <abhaykrn02@gmail.com>
2025-03-20 13:14:25 +0530

    20th March Solved Problems

---
+++ CodeForces/March/20th/2070A.cpp
@@ -0,0 +1,36 @@
+/*     Coded by lazy_abhay     */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int32_t main(void)
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    auto abhay = [&](int __t) -> void
+    {
+        int n;
+        cin >> n;
+        int ans = 3 * (n / 15);
+        int x = n / 15;
+        int y = 15 * x;
+        int z = n - y;
+        ans += min(z + 1, 3LL);
+        cout << ans;
+    };
+
+    int T = 1;
+    cin >> T;
+    for (int t = 1; t <= T; ++t)
+    {
+        abhay(t);
+        cout << endl;
+    }
+    return 0;
+}
