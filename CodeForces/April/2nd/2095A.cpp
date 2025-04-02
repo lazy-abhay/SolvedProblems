a506b4f3b356b88847e69574c7f71b165233d0f5
Abhay <abhaykrn02@gmail.com>
2025-04-02 19:57:40 +0530

    2nd April Solved Problems

---
+++ CodeForces/April/2nd/2095A.cpp
@@ -0,0 +1,29 @@
+/*     Coded by lazy_abhay     */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int32_t main(void)
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    auto abhay = [&](int __t) -> void
+    {
+        cout << "puzzling";
+    };
+
+    int T = 1;
+    // cin >> T;
+    for (int t = 1; t <= T; ++t)
+    {
+        abhay(t);
+        cout << endl;
+    }
+    return 0;
+}
