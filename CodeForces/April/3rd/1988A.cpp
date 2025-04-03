96794da0ab079c27662a4fe2cbccb0883ea77003
Abhay <abhaykrn02@gmail.com>
2025-04-03 12:48:32 +0530

    3rd April Solved Problems

---
+++ CodeForces/April/3rd/1988A.cpp
@@ -0,0 +1,37 @@
+/*     Coded by lazy_abhay     */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int32_t main(void)
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    auto abhay = [&](int __t) -> void
+    {
+        int n, k;
+        cin >> n >> k;
+        int ans = 0;
+        while (n > 1)
+        {
+            ans++;
+            n -= (k - 1);
+        }
+        cout << ans;
+    };
+
+    int T = 1;
+    cin >> T;
+    for (int t = 1; t <= T; ++t)
+    {
+        abhay(t);
+        cout << endl;
+    }
+    return 0;
+}
